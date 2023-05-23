8120d0a063dc9af0d498ec243be00a0ba658d4e9
RahulDurgavajhala <126925086+RahulDurgavajhala@users.noreply.github.com>
2023-05-22 17:48:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var fn="Ranbir"
+var ln="Kapoor"
+var full_name=fn.concat(ln)
+console.log(full_name)
+var a=32
+var b=8
+var add=a+b
+console.log(add)
+var sub=a-b
+console.log(sub)
+var multi=a*b
+console.log(multi)
+var div=a/b
+console.log(div)
